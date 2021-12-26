ac7863150ee6b51d72db29eef37b414cf1d14c93
Spirlhark <Spirlhark@gmail.com>
2021-12-26 18:17:32 +0200

    new_post_send

--- siteblog/blog/tasks.py
+++ siteblog/blog/tasks.py
@@ -1,10 +1,42 @@
+import datetime
+from .models import *
 from django.core.mail import send_mail
 from siteblog.celery import app
 
 from .service import send
-from .models import Contact
 
 
 @app.task
 def send_spam_email(user_email):
     send(user_email)
+
+
+# @app.task
+# def send_new_posts():
+#     today = datetime.date.today()
+#     new_posts = Post.objects.filter(created_at__date=today)
+#     post_new_list = list(new_posts)    .format(post_new_list)
+#     for contact in Contact.objects.all():
+#         send_mail(
+#             'Для наших подписчиков'
+#             'У нас новые пост(ы): {0}',
+#             'x.Spirlhark.x@gmail.com'
+#             [contact.email],
+#             fail_silently=False,
+#         )
+
+
+@app.task
+def send_beat_email():
+    today = datetime.date.today()
+    new_posts = Post.objects.filter(created_at__date=today)
+    post_new_list = list(new_posts)
+    if len(post_new_list) != 0:
+        for user in Contact.objects.all():
+            send_mail(
+                'Вы подписаны на рассылку',
+                'У нас новые пост(ы): {0}'.format(post_new_list),
+                'x.spirlhark.x@gmail.com',
+                [user.email],
+                fail_silently=False,
+            )
