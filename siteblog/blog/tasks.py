399325a95b722aae8fd737dae11fab838339a38e
Spirlhark <Spirlhark@gmail.com>
2022-01-06 11:18:33 +0200

    new_post_send_updates

--- siteblog/blog/tasks.py
+++ siteblog/blog/tasks.py
@@ -1,7 +1,5 @@
 import datetime
-
 from django.core.cache import cache
-
 from .models import *
 from django.core.mail import send_mail
 from siteblog.celery import app
@@ -18,9 +16,11 @@ def send_spam_email(user_email):
 def send_beat_email():
     new_posts = Post.objects.filter(created_at__date__gte=cache.get('last_send') or datetime.date.today())
     cache.set('last_send', datetime.date.today(), None)
+    new_post_links = ['http://127.0.0.1:8000/post/{0}/'.format(x.slug)+'\n' for x in new_posts]
+    msg = '\n'.join(new_post_links)
     [send_mail(
         'У нас новые посты',
-        'http://127.0.0.1:8000/post/{0}/'.format(x.slug),
+        msg,
         'x.Spirlhark.x@gmail.com',
         [contact.email],
-        fail_silently=False,)for x in new_posts for contact in Contact.objects.all() if len(new_posts) != 0]
+        fail_silently=False,) for contact in Contact.objects.all() if len(new_posts) != 0]
